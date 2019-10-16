8ca0845666597056853a3acbf80eda8fa6d4014f
Øystein Gregers Lindbjør <oystein.gregers.lindbjor@betr.io>
2019-10-16 12:28:40 +0200

    Add all files

--- Domain/Features/CheckIn/CheckInChangedEvent.cs
+++ Domain/Features/CheckIn/CheckInChangedEvent.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using MoreLinq;
 using Utilities;
 
 namespace Domain.Features.CheckIn
---
+++ Utilities/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Utilities
+{
+    public static class ObjectExtensions
+    {
+        public static T Clone<T>(this T @object) => JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(@object));
+    }
+
+
+}
